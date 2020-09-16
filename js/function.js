bffe0b70c99adf47f35c7535e1729f36e8580c0a
naphatsakorn <james.11350@gmail.com>
2020-09-16 14:06:02 +0700

    add function file

---
+++ js/function.js
@@ -0,0 +1,21 @@
+function add(a,b) {
+    return a + b 
+}
+
+function mul(a,b){
+    return a * b
+}
+
+function searchCustomer (customerName) {
+    //find some customer
+}
+
+const add = (a,b) => {
+    this.user = 'James'
+    return a + b
+}
+
+console.log(this.user)
+
+console.log(add(2,3))
+console.log(mul(100,3))
