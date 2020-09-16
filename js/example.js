1b8f888d9063afa0f618230bf334b477c4a72d6e
naphatsakorn <james.11350@gmail.com>
2020-09-16 13:38:59 +0700

    test

---
+++ js/example.js
@@ -0,0 +1,26 @@
+console.log("Hello World")
+
+const PI = 3.14 
+console.log("PI = ",PI,22/7)
+
+let name = "John"
+name = "James"
+name = "Beam"
+
+let test ="Hello"
+name = `${test} B`
+
+console.log("name:",name)
+
+let arr = ['apple', 'banana', 'kiwi']
+console.log("arr", arr[1])
+
+let person = {
+    name : 'James',
+    age : 21,
+    gender : {
+        name :"male"
+    },
+    arr
+}
+console.log(person)
