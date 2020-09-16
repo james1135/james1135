1b55cf153fe32064f004b1432181bd1f01288640
naphatsakorn <james.11350@gmail.com>
2020-09-16 15:04:50 +0700

    quiz2file

---
+++ js/quiz2.js
@@ -0,0 +1,13 @@
+for (let i = 1; i <= 20; i += 1) {
+    console.log(i)
+}
+
+for (let i = 1; i <=20 ; i +=1) {
+    console.log(i%2 === 0 ? i : "")
+}
+
+let mul = [-1,2,3,56,6,-93,0,4].map(el => el*2)
+    console.log(mul)
+
+let by = [10,13,44,55,89,0,12,1].filter(el => el%2 == 0)
+    console.log(by)
